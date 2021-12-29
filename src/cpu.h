bd43a9d1029887fc80076332db37edb9d5afdbc4
kamil <kammmilkos@gmail.com>
2021-12-29 12:10:20 +0100

    refactor CPU definition

--- src/cpu.h
+++ src/cpu.h
@@ -6,14 +6,38 @@
 #include "byte.h"
 
 typedef struct Cpu {
-    uint8_t A;
-    uint8_t F;
-    uint8_t B;
-    uint8_t C;
-    uint8_t D;
-    uint8_t E;
-    uint8_t H;
-    uint8_t L;
+    union {
+        struct {
+            uint8_t A;
+            uint8_t F;
+        };
+        uint16_t AF;
+    };
+
+    union {
+        struct {
+            uint8_t B;
+            uint8_t C;
+        };
+        uint16_t BC;
+    };
+
+    union {
+        struct {
+            uint8_t D;
+            uint8_t E;
+        };
+        uint16_t DE;
+    };
+
+    union {
+        struct {
+            uint8_t H;
+            uint8_t L;
+        };
+        uint16_t HL;
+    };
+
     uint16_t PC;
     uint16_t SP;
 } Cpu;
