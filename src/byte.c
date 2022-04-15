6edd076113a3807d01489752185752f000c29baa
kamil <kammmilkos@gmail.com>
2022-04-15 12:51:22 +0200

    carry/borrow bits methods for 16 bit variables

--- src/byte.c
+++ src/byte.c
@@ -1,6 +1,6 @@
 #include "byte.h"
 
-static void get_bits(uint8_t byte, uint8_t* bits);
+static void get_bits(uint16_t value, uint16_t* bits);
 
 uint8_t low_nibble(uint8_t byte) {
     return byte & 0x0f;
@@ -61,34 +61,63 @@ uint8_t get_bit_number(Bit bit) {
             return 6;
         case BIT7:
             return 7;
+        case BIT8:
+            return 8;
+        case BIT9:
+            return 9;
+        case BIT10:
+            return 10;
+        case BIT11:
+            return 11;
+        case BIT12:
+            return 12;
+        case BIT13:
+            return 13;
+        case BIT14:
+            return 14;
+        case BIT15:
+            return 15;
         default:
             return 0;
     }
 }
 
-static void get_bits(uint8_t byte, uint8_t* bits) {
-    bits[0] = byte & BIT0;
-    bits[1] = byte & BIT1;
-    bits[2] = byte & BIT2;
-    bits[3] = byte & BIT3;
-    bits[4] = byte & BIT4;
-    bits[5] = byte & BIT5;
-    bits[6] = byte & BIT6;
-    bits[7] = byte & BIT7;
+static void get_bits(uint16_t value, uint16_t* bits) {
+    bits[0] = value & BIT0;
+    bits[1] = value & BIT1;
+    bits[2] = value & BIT2;
+    bits[3] = value & BIT3;
+    bits[4] = value & BIT4;
+    bits[5] = value & BIT5;
+    bits[6] = value & BIT6;
+    bits[7] = value & BIT7;
+    bits[8] = value & BIT8;
+    bits[9] = value & BIT9;
+    bits[10] = value & BIT10;
+    bits[11] = value & BIT11;
+    bits[12] = value & BIT12;
+    bits[13] = value & BIT13;
+    bits[14] = value & BIT14;
+    bits[15] = value & BIT15;
 }
 
-uint8_t add_get_carry_bit(uint8_t byte1, uint8_t byte2, Bit bit) {
-    uint8_t byte1_bits[8];
-    uint8_t byte2_bits[8];
-    get_bits(byte1, byte1_bits);
-    get_bits(byte2, byte2_bits);
+uint8_t add_get_carry_bit(uint16_t value1, uint16_t value2, Bit bit) {
+    uint16_t value1_bits[16];
+    uint16_t value2_bits[16];
+    get_bits(value1, value1_bits);
+    get_bits(value2, value2_bits);
 
-    // 9 bit carry to simplify logic for the 0 bit carry
-    uint8_t carried_bits[9] = {[0] = 0};
+    // 17 bit carry to simplify logic for the 0 bit carry
+    uint8_t carried_bits[17] = {[0] = 0};
 
-    for (int i = 0; i < 8; i++) {
-        carried_bits[i + 1] = ((byte1_bits[i] && byte2_bits[i]) || (byte1_bits[i] && carried_bits[i]) || (byte2_bits[i] && carried_bits[i]));
+    for (int i = 0; i < 16; i++) {
+        carried_bits[i + 1] = ((value1_bits[i] && value2_bits[i]) || (value1_bits[i] && carried_bits[i]) || (value2_bits[i] && carried_bits[i]));
     }
 
     return carried_bits[get_bit_number(bit) + 1];
+}
+
+uint8_t subtract_get_borrow_bit(uint16_t value1, uint16_t value2, Bit bit) {
+    uint16_t result = value1 - value2;
+    return add_get_carry_bit(value2, result, bit);
 }
--- src/byte.h
+++ src/byte.h
@@ -4,14 +4,22 @@
 #include <stdint.h>
 
 typedef enum Bit {
-    BIT0 = 0x01,
-    BIT1 = 0x02,
-    BIT2 = 0x04,
-    BIT3 = 0x08,
-    BIT4 = 0x10,
-    BIT5 = 0x20,
-    BIT6 = 0x40,
-    BIT7 = 0x80
+    BIT0 = 0x0001,
+    BIT1 = 0x0002,
+    BIT2 = 0x0004,
+    BIT3 = 0x0008,
+    BIT4 = 0x0010,
+    BIT5 = 0x0020,
+    BIT6 = 0x0040,
+    BIT7 = 0x0080,
+    BIT8 = 0x0100,
+    BIT9 = 0x0200,
+    BIT10 = 0x0400,
+    BIT11 = 0x0800,
+    BIT12 = 0x1000,
+    BIT13 = 0x2000,
+    BIT14 = 0x4000,
+    BIT15 = 0x8000,
 } Bit;
 
 uint8_t low_nibble(uint8_t byte);
@@ -25,5 +33,6 @@ void unset_bit(uint8_t* byte_ptr, Bit bit);
 uint16_t ptr_offset(uint16_t greater_ptr, uint16_t lesser_ptr);
 uint8_t ptr_in(uint16_t ptr, uint16_t low_addr, uint16_t high_addr);
 uint8_t get_bit_number(Bit bit);
-uint8_t add_get_carry_bit(uint8_t byte1, uint8_t byte2, Bit bit);
+uint8_t add_get_carry_bit(uint16_t value1, uint16_t value2, Bit bit);
+uint8_t subtract_get_borrow_bit(uint16_t value1, uint16_t value2, Bit bit);
 #endif
